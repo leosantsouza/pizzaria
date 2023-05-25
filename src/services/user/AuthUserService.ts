3f84804cefc385554a1c826575e6a12c11cc781a
leosantsouza <leosantsouza@hotmail.com>
2023-05-25 00:11:58 -0300

    login alth/service,controller

---
+++ src/services/user/AuthUserService.ts
@@ -0,0 +1,17 @@
+
+import prismaClient from "../../prisma";
+
+interface AuthRequest{
+    email: string;
+    password: string;
+}
+
+class AuthUserService{
+    async execute({ email, password }: AuthRequest){
+        console.log(email);
+
+        return { ok: true}
+    }
+}
+
+export { AuthUserService };
