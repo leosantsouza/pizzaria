435bd1fe5d1dd386e8d3694e22731c312492e109
leosantsouza <leosantsouza@hotmail.com>
2023-06-23 22:34:52 -0300

    authenticate and midwares

--- src/server.ts
+++ src/server.ts
@@ -12,17 +12,17 @@ app.use(cors());
 app.use(router);
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-    if (err instanceof Error) {
-        // Se for uma instancia do tipo Error
-        return res.status(400).json({
-            error: err.message
-        })
-    }
-
-    return res.status(500).json({
-        status: 'error',
-        message: 'Internal Server Error'
+  if (err instanceof Error) {
+    // Se for uma instancia do tipo Error
+    return res.status(400).json({
+      error: err.message
     })
+  }
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal Server Error'
+  })
 
 });
 
--- src/services/user/AuthUserService.ts
+++ src/services/user/AuthUserService.ts
@@ -1,56 +1,56 @@
 
 import prismaClient from "../../prisma";
 import { compare } from "bcryptjs";
-import {sign} from 'jsonwebtoken'
+import { sign } from 'jsonwebtoken'
 
 interface AuthRequest {
-    email: string;
-    password: string;
+  email: string;
+  password: string;
 }
 
-class AuthUserService {
-    async execute({ email, password }: AuthRequest) {
-        //Verificar se o email existe
-        const user = await prismaClient.user.findFirst({
-            where: {
-                email: email
-            }
-        })
+class AuthUserService  {
+  async execute({ email, password }: AuthRequest) {
+    //Verificar se o email existe
+    const user = await prismaClient.user.findFirst({
+      where: {
+        email: email
+      }
+    })
 
-        if(!user){
-            throw new Error("Email not found")
-        }
-
-        //Verificar se a senha esta correta.
-       const passwordMatch = await compare(password, user.password)
-
-       if(!passwordMatch){
-        throw new Error("Password incorrect")
-       }
-
-       //Gerar um tokenJWT e devolver os dados do usuario como id, name e emaail.
-       const token = sign(
-        {
-            name: user.name,
-            email: user.email
-        },
-         process.env.JWT_SECRET,
-         {
-            subject: user.id,
-            expiresIn: '30d'
-
-         }
-       )
-       
-
-
-        return {
-            id: user.id,
-            name: user.name,
-            email: user.email,
-            token: token
-        }
+    if (!user) {
+      throw new Error("Email not found")
     }
+
+    //Verificar se a senha esta correta.
+    const passwordMatch = await compare(password, user.password)
+
+    if (!passwordMatch) {
+      throw new Error("Password incorrect")
+    }
+
+    //Gerar um tokenJWT e devolver os dados do usuario como id, name e emaail.
+    const token = sign(
+      {
+        name: user.name,
+        email: user.email
+      },
+      process.env.JWT_SECRET,
+      {
+        subject: user.id,
+        expiresIn: '30d'
+
+      }
+    )
+
+
+
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      token: token
+    }
+  }
 }
 
 export { AuthUserService };
